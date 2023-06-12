87dd113a77f1b6ad87877730d62e3696d0552f18
subhash kumar singh <subh.singh007@gmail.com>
2023-06-12 19:30:08 +0530

    templete gin example

--- templating/gin/main.go
+++ templating/gin/main.go
@@ -6,15 +6,43 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Task struct {
+	ID       int
+	Name     string
+	Status   string
+	Priority int
+}
+
+var (
+	tasks = []Task{
+		{
+			ID:       1,
+			Name:     "Bob",
+			Status:   "Not Started",
+			Priority: 5,
+		}, {
+			ID:       2,
+			Name:     "Steven",
+			Status:   "Done",
+			Priority: 3,
+		}, {
+			ID:       3,
+			Name:     "Rob",
+			Status:   "Review",
+			Priority: 4,
+		},
+	}
+)
+
 func indexPageHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"title": "welcome everyone",
+	c.HTML(http.StatusOK, "task.tmpl", gin.H{
+		"Tasks": tasks,
 	})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/index:", indexPageHandler)
+	router.GET("/viewtasks", indexPageHandler)
 	router.LoadHTMLGlob("template/*")
 	router.Run(":8080")
 }
